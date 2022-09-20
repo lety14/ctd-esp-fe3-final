bf9ff6bec0676eb79e2e3a5ccf580d23847da82d
Leti <leticiapistarelli@gmail.com>
2022-09-20 00:07:40 -0300

    refactor faqs page

--- pages/faqs.page.tsx
+++ pages/faqs.page.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, NextPage } from "next";
+import { NextPage } from "next";
 import React from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -7,17 +7,10 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Head from "next/head";
 import BodySingle from "dh-marvel/components/layouts/body/single/body-single";
-import { faqsData } from "constants/faqs/faqsData";
 import { Box } from "@mui/material";
-import IFaq from "types/IFaq.type";
+import { faqsData } from "constants/faqs/faqsData";
 
-const endpoint = process.env.LOCAL_API_URL;
-
-interface Props {
-  faqs: IFaq[];
-}
-
-const Faqs: NextPage<Props> = ({ faqs }) => {
+const Faqs: NextPage = () => {
   return (
     <>
       <Head>
@@ -29,7 +22,7 @@ const Faqs: NextPage<Props> = ({ faqs }) => {
       </Head>
       <Box px={2} sx={{ maxWidth: 1500 }}>
         <BodySingle title={"Preguntas Frecuentes"}></BodySingle>
-        {faqs.map((faq) => {
+        {faqsData.map((faq) => {
           return (
             <>
               <Accordion key={faq.id}>
@@ -52,16 +45,6 @@ const Faqs: NextPage<Props> = ({ faqs }) => {
       </Box>
     </>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await fetch(`${endpoint}faqs`);
-  const data: IFaq[] = await response.json();
-  return {
-    props: {
-      faqs: data,
-    },
-  };
 };
 
 export default Faqs;
