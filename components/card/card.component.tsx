cb42badb3f865d6672d5c5dcb685e394cd907711
Leti <leticiapistarelli@gmail.com>
2022-09-20 00:02:28 -0300

    create home page and card component

---
+++ components/card/card.component.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { IComic } from "types/IComic.type";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+
+interface Props {
+  comic: IComic;
+}
+
+const CardComponent: FC<Props> = ({ comic }) => {
+  const router = useRouter();
+
+  return (
+    <Card
+    // sx={{ maxWidth: 380 }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="350"
+          image={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+          alt={comic.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {comic.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <NextLink href={`/comics/${comic.id}`}>
+          <Button variant="seeMore">Ver detalles</Button>
+        </NextLink>
+
+        <NextLink href={`/checkout`}>
+          <Button variant="buyCard">COMPRAR</Button>
+        </NextLink>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CardComponent;
